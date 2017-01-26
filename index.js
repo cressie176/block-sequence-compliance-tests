9ea7e0e77d0d19ecde8298b27f8136e3000598c5
Stephen Cresswell <git@stephen-cresswell.net>
2017-01-26 07:49:58 +0000

    Tidy up

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ module.exports = function(blockSequence) {
         t.test('should ensure sequence is created with custom metadata', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.ensure({ name: sequenceName, value: 10, metadata: { info: 'additional info' } }, function(err, sequence) {
+                blockSequence.ensure({ name: sequenceName, value: 11, metadata: { info: 'additional info' } }, function(err, sequence) {
                     err && t.ifError(err)
+                    t.equal(sequence.value, 11, 'value is correct')
                     t.equal(sequence.metadata.info, 'additional info', 'metadata is correct')
                     t.end()
                 })
@@ -31,11 +32,11 @@ module.exports = function(blockSequence) {
         t.test('should allocate a block of ids', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                blockSequence.allocate({ name: sequenceName, size: 12 }, function(err, block) {
                     err && t.ifError(err)
                     t.equal(block.name, sequenceName, 'block name is correct')
                     t.equal(block.next, 1, 'next is correct')
-                    t.equal(block.remaining, 10, 'remaining is correct')
+                    t.equal(block.remaining, 12, 'remaining is correct')
                     t.end()
                 })
             })
@@ -44,13 +45,13 @@ module.exports = function(blockSequence) {
         t.test('should allocate a second block of ids', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                blockSequence.allocate({ name: sequenceName, size: 13 }, function(err, block) {
                     err && t.ifError(err)
-                    blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                    blockSequence.allocate({ name: sequenceName, size: 14 }, function(err, block) {
                         err && t.ifError(err)
                         t.equal(block.name, sequenceName, 'block name is correct')
-                        t.equal(block.next, 11, 'next is correct')
-                        t.equal(block.remaining, 10, 'remaining is correct')
+                        t.equal(block.next, 14, 'next is correct')
+                        t.equal(block.remaining, 14, 'remaining is correct')
                         t.end()
                     })
                 })
@@ -60,7 +61,7 @@ module.exports = function(blockSequence) {
         t.test('should default to a block size of 1', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName}, function(err, block) {
+                blockSequence.allocate({ name: sequenceName }, function(err, block) {
                     err && t.ifError(err)
                     t.equal(block.remaining, 1, 'remaining is correct')
                     t.end()
@@ -71,12 +72,12 @@ module.exports = function(blockSequence) {
         t.test('should force lowercase sequence names when allocating a block', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                blockSequence.allocate({ name: sequenceName, size: 15 }, function(err, block) {
                     err && t.ifError(err)
-                    blockSequence.allocate({ name: sequenceName.toUpperCase(), size: 10 }, function(err, block) {
+                    blockSequence.allocate({ name: sequenceName.toUpperCase(), size: 16 }, function(err, block) {
                         err && t.ifError(err)
                         t.equal(block.name, sequenceName, 'block name is correct')
-                        t.equal(block.next, 11, 'next is correct')
+                        t.equal(block.next, 16, 'next is correct')
                         t.end()
                     })
                 })
@@ -93,8 +94,9 @@ module.exports = function(blockSequence) {
         t.test('should return custom block metadata', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName, size: 10, metadata: { info: 'additional info' } }, function(err, block) {
+                blockSequence.allocate({ name: sequenceName, size: 17, metadata: { info: 'additional info' } }, function(err, block) {
                     err && t.ifError(err)
+                    t.equal(block.next, 1, 'next is correct')
                     t.equal(block.metadata.info, 'additional info', 'metadata is correct')
                     t.end()
                 })
@@ -104,7 +106,7 @@ module.exports = function(blockSequence) {
         t.test('should omit sequence value', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                blockSequence.allocate({ name: sequenceName, size: 18 }, function(err, block) {
                     err && t.ifError(err)
                     t.notOk(block.hasOwnProperty('value'), 'sequence value was not omitted')
                     t.end()
@@ -115,12 +117,27 @@ module.exports = function(blockSequence) {
         t.test('should allocate blocks atomicly', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                var blocks = 0
+                var blocks = []
                 for (var i = 0; i < 1000; i++) {
-                    blockSequence.allocate({ name: sequenceName, size: 10 }, function(err, block) {
+                    blockSequence.allocate({ name: sequenceName, size: 19 }, function(err, block) {
                         err && t.ifError(err)
-                        if (++blocks === 1000) {
-                            t.equal(block.next, 9991, 'next is correct')
+                        blocks.push(block.next)
+                        if (blocks.length === 1000) {
+                            blocks.sort(function sortNumber(a, b) {
+                                return a - b;
+                            });
+
+                            var passed = true
+                            for (var i = 0; i < 1000; i++) {
+                                var actual = blocks[i]
+                                var expected = (i * 19) + 1
+                                if (actual !== expected) {
+                                    t.fail('Block ' + i + ' was ' + actual + ' but should have been ' + expected)
+                                    passed = false
+                                }
+                            }
+                            if (passed) t.pass('OK')
+
                             t.end()
                         }
                     })
@@ -131,30 +148,14 @@ module.exports = function(blockSequence) {
         t.test('should not redefine a sequence', function(t) {
             blockSequence.remove({ name: sequenceName }, function(err) {
                 err && t.ifError(err)
-                blockSequence.ensure({ name: sequenceName, value: 10 }, function(err, sequence) {
+                blockSequence.ensure({ name: sequenceName, value: 20 }, function(err, sequence) {
                     err && t.ifError(err)
-                    blockSequence.ensure({ name: sequenceName, value: 1 }, function(err, sequence) {
+                    blockSequence.ensure({ name: sequenceName, value: 21 }, function(err, sequence) {
                         err && t.ifError(err)
-                        t.equal(sequence.value, 10, 'value is correct')
+                        t.equal(sequence.value, 20, 'value is correct')
                         t.end()
                     })
                 })
-            })
-        })
-
-        t.test('should ensure sequences atomicly', function(t) {
-            blockSequence.remove({ name: sequenceName }, function(err) {
-                err && t.ifError(err)
-                var blocks = 0
-                for (var i = 0; i < 1000; i++) {
-                    blockSequence.ensure({ name: sequenceName, value: 100 }, function(err, block) {
-                        err && t.ifError(err)
-                        if (++blocks === 1000) {
-                            t.equal(block.value, 100, 'value is correct')
-                            t.end()
-                        }
-                    })
-                }
             })
         })
 
